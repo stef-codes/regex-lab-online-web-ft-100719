04bdf2b6253b0d2c0aa7c67c96d5ef8b6ecb0a36
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-13 02:42:16 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/\A[A-Z]\w*\W\b/)
+  text.match(/\A[A-Z]\w*\W\b/) != nil
 end
 
 def valid_phone_number?(phone)
